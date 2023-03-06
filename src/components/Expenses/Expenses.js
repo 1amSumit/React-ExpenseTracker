b5219444a1d734831b577e9a5a539b401f416785
1amSumit <sj779619@gmail.com>
2023-03-06 16:05:41 +0530

    Conditionally rendered elements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filterdYear, setFilterdYear] = useState("2023");
@@ -13,17 +13,11 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filterdYear;
   });
+
   return (
     <Card className="expenses">
       <ExpensesFilter value={filterdYear} getSelectedYear={selectedYear} />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
